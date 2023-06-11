55c21b0057234ae266d75849ab67c82663c06a48
AfterNath <78166954+AfterNath@users.noreply.github.com>
2023-06-11 09:53:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     app: {
         px: '*',
-        token: 'MTEwOTE0ODUwMTg5MzQ2NDA2NQ.G0MPip.OV192CJHT97eoZWQEzBGh5EpYj6W8l9vQAObVU',
-        owners: '967773487450173490',
-        funny: '967773487450173490',
-        color: 'GREEN',
-        footer: 'AFTERNATH © 2023',
-        maxserver: '10',
+        token: 'TOKEN',
+        owners: 'OWNERID',
+        funny: 'OWNERID',
+        color: 'RANDOM',
+        footer: 'NOM © ANEE',
+        maxserver: '99',
         everyoneMention: false,
         hostedBy: true,
         }
